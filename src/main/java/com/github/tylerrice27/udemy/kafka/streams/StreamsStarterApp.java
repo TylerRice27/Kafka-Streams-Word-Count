22e39c4a598028e33303fc22fea49d0f9753111e
TylerRice27 <Tylerrayrice2@gmail.com>
2023-08-06 19:50:24 -0600

    File Setup and Hello Wold printed to the System Log

---
+++ src/main/java/com/github/tylerrice27/udemy/kafka/streams/StreamsStarterApp.java
@@ -0,0 +1,8 @@
+package com.github.tylerrice27.udemy.kafka.streams;
+
+public class StreamsStarterApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Wold");
+
+}
+}

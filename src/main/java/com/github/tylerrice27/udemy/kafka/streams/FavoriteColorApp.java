c0090c0fb505855531736dfe0666a706ccc469f7
TylerRice27 <Tylerrayrice2@gmail.com>
2023-08-11 20:38:14 -0600

    Created Stream, Performed operations and then wrote it to a new topic

--- src/main/java/com/github/tylerrice27/udemy/kafka/streams/FavoriteColorApp.java
+++ src/main/java/com/github/tylerrice27/udemy/kafka/streams/FavoriteColorApp.java
@@ -8,6 +8,7 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class FavoriteColorApp {
@@ -19,30 +20,35 @@ public class FavoriteColorApp {
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    }
 
-    StreamsBuilder builder = new StreamsBuilder();
+//        We disable the cache to demonstrate all the "steps" involved in the transformation of data - not recommended in prod
+        config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+
+
+        StreamsBuilder builder = new StreamsBuilder();
 
 //    1- Read a KStream
-KStream<String, String> favoriteColorInput = builder.stream("color-input");
-//    2 - Filter Bad Values
-KStream<String, String> onlyColors = favoriteColorInput.filter((key, value) -> value == "")
-//    3 - SelectKey that will be the user Id
-        .selectKey((ignoredKey, newKey) -> newKey)
-//    4 - MapValues to extract the color (as lowercase)
-        .mapValues(onlyColor -> onlyColor.toLowerCase())
-//    5 - Filter to remove bad colors. NOTE come back to this need to filter more
-        .filter((key, value) -> value == "red");
+        KStream<String, String> favoriteColorInput = builder.stream("color-input");
+        KStream<String, String> onlyColors = favoriteColorInput
+//    2 - Filter Bad Values, Must have a , This is good so your data doesnt crash
+                .filter((key, value) -> value.contains(","))
+//    3 - SelectKey that will be the user Id. This tells it which word is the key
+                .selectKey((key, value) -> value.split(",")[0].toLowerCase())
+//    4 - MapValues we get the color from the value and (lowercase for safety). We also split on the , and choose
+//        one index to right
+                .mapValues(value -> value.split(",")[1].toLowerCase())
+//    5 - Filter to remove bad colors. (could be a data sanitization step)
+                .filter((userAsAKey, valueAsAColor) -> Arrays.asList("red", "blue", "green").contains(valueAsAColor));
 //    6 - Write to Kafka as intermediary topic
-    onlyColors.stream.to("middle-topic");
+        onlyColors.to("user-key-and-color");
 //    7 - Read from Kafka as a KTable (KTable)
 
-KTable<String, String> compactedColors = builder.table("middle-topic")
-//    8 - GroupBy colors
-        .groupBy(key,value)
-//    9 - Count to count colors occurrences (KTable)
-        .count()
-//    10 - Write to Kafka as final topic
-        table.to("color-output")
-
+//KTable<String, String> compactedColors = builder.table("middle-topic")
+////    8 - GroupBy colors
+//        .groupBy(key,value)
+////    9 - Count to count colors occurrences (KTable)
+//        .count()
+////    10 - Write to Kafka as final topic
+//        table.to("color-output")
+    }
 }

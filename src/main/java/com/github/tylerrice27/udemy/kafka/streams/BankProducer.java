66c795947fe9a11ece831ebde2f00686906fb754
TylerRice27 <Tylerrayrice2@gmail.com>
2023-08-22 16:50:36 -0600

    Updated Producer with random number to add to different people accounts between 0 and 100

--- src/main/java/com/github/tylerrice27/udemy/kafka/streams/BankProducer.java
+++ src/main/java/com/github/tylerrice27/udemy/kafka/streams/BankProducer.java
@@ -1,13 +1,16 @@
 package com.github.tylerrice27.udemy.kafka.streams;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.serialization.DoubleSerializer;
-import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.rocksdb.Transaction;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import java.time.Instant;
 import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class BankProducer {
@@ -17,47 +20,57 @@ public class BankProducer {
 
         Properties config = new Properties();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+//        This enables Exactly Once Delivery Semantics
         config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, DoubleSerializer.class.getName());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         config.put(ProducerConfig.ACKS_CONFIG, "all");
-
-        KafkaProducer<String,Double> producer = new KafkaProducer<>(config);
-
-
-
-
-        for (int i = 0; i < 100 ; i++) {
-            String topic = "BankTopic";
-            String key = "Tyler";
-            double value = Math.round(Math.random()*700);
-
-            ProducerRecord<String, Double> moneyMessage = new ProducerRecord<>(topic, key, value);
+//        Three retries in case something goes wrong so I dont loose data
+        config.put(ProducerConfig.RETRIES_CONFIG, "3");
+//        Linger MS is wait time. This is not great for production but this will send data very fast
+        config.put(ProducerConfig.LINGER_MS_CONFIG, "1");
 
 
-            producer.send(moneyMessage, new Callback() {
+        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
 
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    if (e == null) {
-                        log.info("Key:" + key + "|Value:" + value);
-                    } else {
-                        log.error("Error while producing the random bank number");
-                    }
-                }
-            });
+
+//            String topic = "BankTopic";
+//            String key = "Tyler";
+//            double value = Math.round(Math.random()*700);
+
+//        My main loop that produces the message
+        int i = 0;
+        while (true) {
             try {
-                Thread.sleep(500);
+                producer.send(randomTransaction("Tyler"));
+                Thread.sleep(100);
+                producer.send(randomTransaction("David"));
+                Thread.sleep(100);
+                producer.send(randomTransaction("Mike"));
+                i += 1;
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                break;
             }
 
         }
         producer.flush();
-
         producer.close();
-
     }
 
+        private static ProducerRecord<String,String> randomTransaction(String name){
+//            Creates an empty JSON {} because of the dependancy library I brought in
+            ObjectNode transaction = JsonNodeFactory.instance.objectNode();
+//            This creates a random transaction 0 to 100
+            Integer amount = ThreadLocalRandom.current().nextInt(0,100);
 
-}
+//            Instant.now() is to get the current time using Java
+            Instant now = Instant.now();
+//            Write the data to the JSON document
+            transaction.put("name", name);
+            transaction.put("amount", amount);
+            transaction.put("time", now.toString());
+//           Return the message to the topic, key, transaction == value which is a JSON then toString()
+            return new ProducerRecord<>("TestBankTopic", name, transaction.toString());
+        }
+
+    }
---
+++ src/test/java/com/github/tylerrice27/udemy/kafka/streams/BankProducer.java
@@ -0,0 +1,4 @@
+package com.github.tylerrice27.udemy.kafka.streams;
+
+public class BankProducer {
+}

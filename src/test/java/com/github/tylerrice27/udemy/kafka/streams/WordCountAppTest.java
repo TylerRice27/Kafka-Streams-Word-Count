25f1d6ca2ad12a4a55cca2c2fb18ba6c84360121
TylerRice27 <Tylerrayrice2@gmail.com>
2023-08-24 14:36:46 -0600

    Imported Testing Dependency and did a practice test

---
+++ src/test/java/com/github/tylerrice27/udemy/kafka/streams/WordCountAppTest.java
@@ -0,0 +1,14 @@
+package com.github.tylerrice27.udemy.kafka.streams;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WordCountAppTest {
+
+    @Test
+    public void dummyTest(){
+        String dummy = "Du" + "mmy";
+        assertEquals(dummy, "Dummy");
+    }
+}

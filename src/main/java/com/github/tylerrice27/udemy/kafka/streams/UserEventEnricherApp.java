cbde596e3f19a0e8d040a2c25829a9a8590975cf
TylerRice27 <Tylerrayrice2@gmail.com>
2023-08-24 00:32:39 -0600

    Streams Configs

--- src/main/java/com/github/tylerrice27/udemy/kafka/streams/UserEventEnricherApp.java
+++ src/main/java/com/github/tylerrice27/udemy/kafka/streams/UserEventEnricherApp.java
@@ -1,4 +1,30 @@
 package com.github.tylerrice27.udemy.kafka.streams;
 
+import jdk.nashorn.internal.objects.Global;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+
+import java.util.Properties;
+
 public class UserEventEnricherApp {
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-event-enricher-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+
+
+
+    }
+
+
 }
